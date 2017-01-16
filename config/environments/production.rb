bb5f74bbf318345b0d6fbe0a2302591e53e5656d
susan <smashevich@gmail.com>
2017-01-16 11:18:54 -0800

    uncommented action_mailer default_url_options in config/env/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  # config.action_mailer.default_url_options = {host: 'https://safe-lake-30538.herokuapp.com'}
+  config.action_mailer.default_url_options = {host: 'https://safe-lake-30538.herokuapp.com'}
   # config.action_mailer.delivery_method = :smtp
   # ActionMailer::Base.smtp_settings = {
   #   :address => 'smtp.sendgrid.net',
